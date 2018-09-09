977a1e8b3dc2a0c1dee90c9afb7de6e54d8c17db
Noemi Madrid <noemimadrid@noemis-macbook-pro.local>
2018-09-08 17:31:34 -0700

    Initial Commit

---
+++ LiveCameraTest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LiveCameraTest
+//
+//  Created by Noemi Madrid on 9/8/18.
+//  Copyright © 2018 Noemi Madrid. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LiveCameraTest/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LiveCameraTest
+//
+//  Created by Noemi Madrid on 9/8/18.
+//  Copyright © 2018 Noemi Madrid. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
