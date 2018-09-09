9a678eb6f730cc97a8ae5e71d934b6e80b531cdb
Noemi Madrid <noemimadrid@noemis-macbook-pro.local>
2018-09-09 13:36:03 -0700

    Data persisted using UserDefaultMethods

--- LiveCameraTest/ToDoListViewController.swift
+++ LiveCameraTest/ToDoListViewController.swift
@@ -11,11 +11,19 @@ import UIKit
 class ToDoListViewController: UITableViewController {
     
     var itemArray = ["Pick Party Theme", "Send Invitations","Decorate", "Food", "Party Games", "Party Venue"]
+    let defaults = UserDefaults.standard
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items =  defaults.array(forKey: "ToDoListArray") as? [String]{
+            
+            itemArray = items
+        }
+        
 
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -68,6 +76,7 @@ class ToDoListViewController: UITableViewController {
             
             
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
             
             
